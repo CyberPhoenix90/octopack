6e92c4617b4a1b916d2af6abfdc7eb080343bb33
bawierino <x.hofmann91@gmail.com>
2019-12-31 00:02:43 +0100

    actually set up main and typings properly

--- projects/libraries/logger/dist/index.js
+++ projects/libraries/logger/dist/index.js
@@ -1,1 +1,4 @@
-export { Logger } from './logger';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var logger_1 = require("./logger");
+exports.Logger = logger_1.Logger;
