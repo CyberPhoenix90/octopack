538619160232fde1e151f4688e62640b087188a0
CyberPhoenix <joao260690@gmail.com>
2020-01-05 23:56:11 +0100

    clean up search space by removing a bunch of generated code from search

--- projects/business_logic/plugins/meta_programing/src/meta_programing.js
+++
@@ -1,9 +0,0 @@
-/**
-#generator(generate, fileMatcher) {
-    const files = fileMatcher.match('**\/*', {
-        excludeSelf:true
-    })
-    files.map(f => generate(`export * from ${f.relative()}`))
-}
- */
-//# sourceMappingURL=meta_programing.js.map
