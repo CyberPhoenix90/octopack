d0d7ff3a3a74589fc83be36fde656d9e0d868527
CyberPhoenix <joao260690@gmail.com>
2020-01-08 19:00:40 +0100

    improve npm install plugin to detect when node modules are up to date

--- projects/business_logic/plugins/npm_installer/dist/npm.js
+++ projects/business_logic/plugins/npm_installer/dist/npm.js
@@ -1,46 +1,78 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
+const path_1 = require("path");
 function npmInstall(args) {
     return async (model, context) => {
-        context.uiLogger.info(`[${model.project.resolvedConfig.name}]Installing npm dependencies`);
-        await install(model.project);
+        if (await context.fileSystem.exists(path_1.join(model.project.path, 'package.json'))) {
+            const pkg = JSON.parse(await context.fileSystem.readFile(path_1.join(model.project.path, 'package.json'), 'utf8'));
+            if (pkg.dependencies &&
+                Object.keys(pkg.dependencies).length > 0 &&
+                !(await isUpToDate(model, context, pkg))) {
+                context.uiLogger.info(`[${model.project.resolvedConfig.name}]Installing npm dependencies`);
+                await install(model.project);
+            }
+        }
         return model;
     };
 }
 exports.npmInstall = npmInstall;
-function install(project, isRetry = false) {
-    return new Promise((resolve, reject) => {
-        const handle = child_process_1.spawn('npm', ['install'], {
-            cwd: project.path
-        });
-        const stdBuffer = [];
-        handle.stdout.on('data', (msg) => {
-            stdBuffer.push(msg);
-        });
-        handle.stderr.on('data', (msg) => {
-            stdBuffer.push(msg);
-        });
-        handle.on('error', (err) => {
-            reject(err);
-        });
-        handle.on('close', () => {
-            resolve();
-        });
-        handle.on('exit', (code) => {
-            if (code !== 0) {
-                if (isRetry) {
-                    console.error(stdBuffer.join(''));
-                    reject(code);
-                }
-                else {
-                    install(project, true).then(resolve, reject);
+async function isUpToDate(model, context, pkg) {
+    if (await context.fileSystem.exists(path_1.join(model.project.path, 'package-lock.json'))) {
+        const lockFile = JSON.parse(await context.fileSystem.readFile(path_1.join(model.project.path, 'package-lock.json'), 'utf8'));
+        for (const dep of Object.keys(pkg.dependencies)) {
+            if (context.fileSystem.exists(path_1.join(model.project.path, 'node_modules', dep, 'package.json'))) {
+                const depPkg = JSON.parse(await context.fileSystem.readFile(path_1.join(model.project.path, 'node_modules', dep, 'package.json'), 'utf8'));
+                if (depPkg.version !== lockFile.dependencies[dep].version) {
+                    return false;
                 }
             }
             else {
-                resolve();
+                return false;
             }
+        }
+    }
+    else {
+        return false;
+    }
+    return true;
+}
+async function install(project, isRetry = false) {
+    await runCommand('npm', ['install']);
+    await runCommand('npm', ['install', '--package-lock-only']);
+    function runCommand(cli, args) {
+        return new Promise((resolve, reject) => {
+            const handle = child_process_1.spawn(cli, args, {
+                cwd: project.path
+            });
+            const stdBuffer = [];
+            handle.stdout.on('data', (msg) => {
+                stdBuffer.push(msg);
+            });
+            handle.stderr.on('data', (msg) => {
+                stdBuffer.push(msg);
+            });
+            handle.on('error', (err) => {
+                reject(err);
+            });
+            handle.on('close', () => {
+                resolve();
+            });
+            handle.on('exit', (code) => {
+                if (code !== 0) {
+                    if (isRetry) {
+                        console.error(stdBuffer.join(''));
+                        reject(code);
+                    }
+                    else {
+                        install(project, true).then(resolve, reject);
+                    }
+                }
+                else {
+                    resolve();
+                }
+            });
         });
-    });
+    }
 }
 //# sourceMappingURL=npm.js.map
--- projects/business_logic/plugins/npm_installer/src/npm.ts
+++ projects/business_logic/plugins/npm_installer/src/npm.ts
@@ -1,48 +1,87 @@
 import { Project, OctoPackBuildPlugin, ProjectBuildData, ScriptContext } from 'models';
 import { spawn } from 'child_process';
 import { MapLike } from '../../../../../typings/common';
+import { join } from 'path';
 
 export function npmInstall(args: MapLike<any>): OctoPackBuildPlugin {
 	return async (model: ProjectBuildData, context: ScriptContext) => {
-		context.uiLogger.info(`[${model.project.resolvedConfig.name}]Installing npm dependencies`);
-		await install(model.project);
+		if (await context.fileSystem.exists(join(model.project.path, 'package.json'))) {
+			const pkg = JSON.parse(await context.fileSystem.readFile(join(model.project.path, 'package.json'), 'utf8'));
+
+			if (
+				pkg.dependencies &&
+				Object.keys(pkg.dependencies).length > 0 &&
+				!(await isUpToDate(model, context, pkg))
+			) {
+				context.uiLogger.info(`[${model.project.resolvedConfig.name}]Installing npm dependencies`);
+				await install(model.project);
+			}
+		}
 		return model;
 	};
 }
 
-function install(project: Project, isRetry: boolean = false): Promise<void> {
-	return new Promise((resolve, reject) => {
-		const handle = spawn('npm', ['install'], {
-			cwd: project.path
-		});
+async function isUpToDate(model: ProjectBuildData, context: ScriptContext, pkg: any): Promise<boolean> {
+	if (await context.fileSystem.exists(join(model.project.path, 'package-lock.json'))) {
+		const lockFile = JSON.parse(
+			await context.fileSystem.readFile(join(model.project.path, 'package-lock.json'), 'utf8')
+		);
+		for (const dep of Object.keys(pkg.dependencies)) {
+			if (context.fileSystem.exists(join(model.project.path, 'node_modules', dep, 'package.json'))) {
+				const depPkg = JSON.parse(
+					await context.fileSystem.readFile(
+						join(model.project.path, 'node_modules', dep, 'package.json'),
+						'utf8'
+					)
+				);
 
-		const stdBuffer: string[] = [];
-		handle.stdout.on('data', (msg) => {
-			stdBuffer.push(msg);
-		});
-		handle.stderr.on('data', (msg) => {
-			stdBuffer.push(msg);
-		});
-
-		handle.on('error', (err) => {
-			reject(err);
-		});
-
-		handle.on('close', () => {
-			resolve();
-		});
-
-		handle.on('exit', (code) => {
-			if (code !== 0) {
-				if (isRetry) {
-					console.error(stdBuffer.join(''));
-					reject(code);
-				} else {
-					install(project, true).then(resolve, reject);
+				if (depPkg.version !== lockFile.dependencies[dep].version) {
+					return false;
 				}
 			} else {
-				resolve();
+				return false;
 			}
+		}
+	} else {
+		return false;
+	}
+	return true;
+}
+
+async function install(project: Project, isRetry: boolean = false): Promise<void> {
+	await runCommand('npm', ['install']);
+	await runCommand('npm', ['install', '--package-lock-only']);
+
+	function runCommand(cli: string, args: string[]): Promise<void> {
+		return new Promise((resolve, reject) => {
+			const handle = spawn(cli, args, {
+				cwd: project.path
+			});
+			const stdBuffer: string[] = [];
+			handle.stdout.on('data', (msg) => {
+				stdBuffer.push(msg);
+			});
+			handle.stderr.on('data', (msg) => {
+				stdBuffer.push(msg);
+			});
+			handle.on('error', (err) => {
+				reject(err);
+			});
+			handle.on('close', () => {
+				resolve();
+			});
+			handle.on('exit', (code) => {
+				if (code !== 0) {
+					if (isRetry) {
+						console.error(stdBuffer.join(''));
+						reject(code);
+					} else {
+						install(project, true).then(resolve, reject);
+					}
+				} else {
+					resolve();
+				}
+			});
 		});
-	});
+	}
 }
